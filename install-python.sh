ae3adc0e9c4f272fd4f48f2a265582f2c5088e98
Jude Zhu <Jude.x.zhu@gmail.com>
2017-05-27 09:32:15 +0800

    Create install-python.sh

---
+++ install-python.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -uxe
+
+VERSION=2.7.13.2713
+PACKAGE=ActivePython-${VERSION}-linux-x86_64-glibc-2.3.6-401785
+
+# make directory
+mkdir -p /opt/bin
+cd /opt
+
+wget http://downloads.activestate.com/ActivePython/releases/${VERSION}/${PACKAGE}.tar.gz
+tar -xzvf ${PACKAGE}.tar.gz
+
+mv ${PACKAGE} apy && cd apy && ./install.sh -I /opt/python/
+
+ln -sf /opt/python/bin/easy_install /opt/bin/easy_install
+ln -sf /opt/python/bin/pip /opt/bin/pip
+ln -sf /opt/python/bin/python /opt/bin/python
+ln -sf /opt/python/bin/python /opt/bin/python2
+ln -sf /opt/python/bin/virtualenv /opt/bin/virtualenv
